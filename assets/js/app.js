261b13040e8a71658d43e826b6b41cc435c4c5a4
Franco Brandan <brandanfranco0@gmail.com>
2021-12-06 21:41:45 +0100

    Se agregan Funciones para mezclar acrtas y extraer valor de carta

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+let deck = [];
+const tipos = ["C", "D", "H", "S"];
+const especiales = ["A", "J", "Q", "K"];
+
+const crearDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
+    }
+  }
+
+  for (let tipo of tipos) {
+    for (let especial of especiales) {
+      deck.push(especial + tipo);
+    }
+  }
+
+  console.log(deck);
+
+  deck = _.shuffle(deck);
+  console.log(deck);
+  return deck;
+};
+
+crearDeck();
+
+const getCard = () => {
+  if (deck.length === 0) {
+    throw " No hay mas cartas en el  deck ";
+  }
+  const card = deck.pop();
+  console.log(card);
+
+  return card;
+};
+
+// getCard();
+
+const valorCard = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
+
+const valor = valorCard(getCard());
+console.log(valor);
