1de06b6a61aa7126ff639912d667c386445d9325
Franco Brandan <brandanfranco0@gmail.com>
2021-12-08 18:20:26 +0100

    Se agrega  los eventos a los botones de stop y new Game y se agrega funcion para el turno de la computadora

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,7 @@ const crearDeck = () => {
 
   deck = _.shuffle(deck);
   console.log(deck);
+
   return deck;
 };
 
@@ -29,7 +30,6 @@ const getCard = () => {
     throw " No hay mas cartas en el  deck ";
   }
   const card = deck.pop();
-  console.log(card);
 
   return card;
 };
@@ -41,5 +41,88 @@ const valorCard = (carta) => {
   return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
 };
 
-const valor = valorCard(getCard());
-console.log(valor);
+const btnGetCard = document.querySelector("#btnGetCard");
+const small = document.querySelectorAll("small");
+const gamerDiv = document.querySelector("#gamer-cards");
+const computerDiv = document.querySelector("#computer-cards");
+const btnStop = document.querySelector("#btnStop");
+const btnNewGame = document.querySelector("#btnNewGame");
+
+let gamerPoints = 0;
+let computerPoints = 0;
+
+btnGetCard.addEventListener("click", () => {
+  const card = getCard();
+  gamerPoints = gamerPoints + valorCard(card);
+  console.log(gamerPoints);
+  small[0].innerText = gamerPoints;
+
+  const imgCard = document.createElement("img");
+  imgCard.src = `./cartas/${card}.png`;
+  imgCard.classList.add("cards");
+  gamerDiv.append(imgCard);
+
+  if (gamerPoints > 21) {
+    console.log("Lo siento perdiste, intentalo de nuevo");
+    btnGetCard.disabled = true;
+    btnStop.disabled = true;
+    computerturn(gamerPoints);
+  } else if (gamerPoints === 21) {
+    console.warn("21, Genial bien echo ");
+    btnGetCard.disabled = true;
+    btnStop.disabled = true;
+    computerturn(gamerPoints);
+  }
+});
+
+const computerturn = (points) => {
+  do {
+    const card = getCard();
+    computerPoints = computerPoints + valorCard(card);
+
+    small[1].innerText = computerPoints;
+
+    const imgCard = document.createElement("img");
+    imgCard.src = `./cartas/${card}.png`;
+    imgCard.classList.add("cards");
+    computerDiv.append(imgCard);
+
+    if (points > 21) {
+      break;
+    }
+  } while (computerPoints < points && points <= 21);
+
+  setTimeout(() => {
+    if (gamerPoints <= 21 && gamerPoints > computerPoints) {
+      alert("felicitaciones Ganaste");
+    } else if ((computerPoints <= 21) & (computerPoints > gamerPoints)) {
+      alert("Computadora Gano ");
+    } else if (gamerPoints === computerPoints) {
+      alert("Nadie Gano");
+    } else if (computerPoints > 21) {
+      alert("Felictaciones Ganaste");
+    } else if (gamerPoints > 21) {
+      alert("Lo siento perdiste, intentalo de nuevo");
+    }
+  }, 50);
+};
+
+btnStop.addEventListener("click", () => {
+  btnGetCard.disabled = true;
+  btnStop.disabled = true;
+  computerturn(gamerPoints);
+});
+
+btnNewGame.addEventListener("click", () => {
+  console.clear();
+  let newDeck = [];
+  newDeck = crearDeck();
+  gamerPoints = 0;
+  computerPoints = 0;
+  small[0].innerText = 0;
+  small[1].innerText = 0;
+  btnGetCard.disabled = false;
+  btnStop.disabled = false;
+  gamerDiv.innerText = "";
+  computerDiv.innerText = "";
+});
